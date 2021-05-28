4df9bd038ed7bd466b0b104aac297bc759202ddc
Christopher Inman <christophereinman@gmail.com>
2021-05-27 19:28:10 -0500

    Added refs, forwardrefs and cart items

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import MealItemForm from './MealItemForm';
 import styles from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const mealData = props.meal;
   const price = `$${mealData.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: mealData.id,
+      name: mealData.name,
+      amount: mealData.amount,
+      price: mealData.price,
+    });
+  };
 
   return (
     <li className={styles.meal}>
@@ -15,7 +27,7 @@ const MealItem = (props) => {
       <div className={styles.description}>{mealData.description}</div>
       <div className={styles.price}>{price}</div>
       <div>
-        <MealItemForm id={mealData.id} />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import styles from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={styles.input}>
       <label htmlFor={props.input.id} className={styles.label}>
         {props.label}
       </label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
